28672e2a00834ae6b4a0421b7ae0a84e400f5eea
Andrew Allbright <aallbrig@gmail.com>
2023-05-29 16:04:59 -0400

    Add chromakey processor to down.sh

--- apps/webapp-frontend/src/config.js
+++ apps/webapp-frontend/src/config.js
@@ -10,12 +10,12 @@
 
 export const appConfig = {
   // MODULE 1- BACKEND
-  initStateAPI: 'https://svs05cpsxj.execute-api.us-east-2.amazonaws.com/Prod/InitState/', // e.g. https://12abcdef89.execute-api.us-west-2.amazonaws.com/Prod/InitState/
+  initStateAPI: '', // e.g. https://12abcdef89.execute-api.us-west-2.amazonaws.com/Prod/InitState/
   // MODULE 2 - REALTIME
   iot: {
-    poolId: 'us-east-2:ff498477-25ad-44cb-814a-a3d921004d29', // e.g. 'us-west-2:1abcdef-1234-abcd-1234-abcde123456'
-    host: 'a38clbj65f7vj5-ats.iot.us-east-2.amazonaws.com', // e.g. 'ab12ab12abcde.iot.us-east-1.amazonaws.com'
-    region: 'us-east-2' // e.g. 'us-west-1'
+    poolId: '', // e.g. 'us-west-2:1abcdef-1234-abcd-1234-abcde123456'
+    host: '', // e.g. 'ab12ab12abcde.iot.us-east-1.amazonaws.com'
+    region: '' // e.g. 'us-west-1'
   },
   // MODULE 3 - PHOTOS
   photoUploadURL: '', // e.g. https://12abcdfg89.execute-api.us-west-2.amazonaws.com/Prod/Upload
